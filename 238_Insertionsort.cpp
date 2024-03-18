6e00e6f930ef0224f523f0a7be4802b33536c26c
husnakamilaa <husna.kamila.ft23@mail.umy.ac.id>
2024-03-18 19:17:31 +0700

    membuat prosedur input

--- 238_Insertionsort.cpp
+++ 238_Insertionsort.cpp
@@ -6,3 +6,26 @@ using namespace std;
 
 int arr[40];
 int n;
+
+void input() {
+	while (true) {
+		cout << "Masukkan banyaknya elemen pada array: ";
+		cin >> n;
+		if (n <= 40)
+			break;
+		else {
+			cout << "\nArray dapat mempunyai maksimal 40 elemen.\n";
+
+		}
+	}
+	cout << endl;
+	cout << "======================" << endl;
+	cout << "Masukkan elemen array: " << endl;
+	cout << "======================" << endl;
+
+	for (int i = 0; i < n; i++) {
+		cout << "Data ke-" << (i + 1) << ":";
+		cin >> arr[i]
+	}
+
+}
