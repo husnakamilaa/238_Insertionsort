6c7ff3821468daa1fa941d32a6641d535f9b94ea
husnakamilaa <husna.kamila.ft23@mail.umy.ac.id>
2024-03-18 19:32:30 +0700

    melanjutkan prosedur proses insertion sort

--- 238_Insertionsort.cpp
+++ 238_Insertionsort.cpp
@@ -32,4 +32,18 @@ void input() {
 
 void insertionSort() {
 	int i, j, temp;
+
+	for (i = 1; i <= n - 1; i++) { //step 1
+		temp = arr[i]; //step 2
+
+		j = i - 1; //step 3
+
+		while (j >= 0 && arr[j] > temp) //step 4
+		{
+			arr[j + 1] = arr[j]; //step 4a
+			j--; //step 4b
+		}
+
+		arr[j + 1] = temp; //step 5
+	}
 }
