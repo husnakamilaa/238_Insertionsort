b93f2fbb05b854104d44235b16a633071673be38
husnakamilaa <husna.kamila.ft23@mail.umy.ac.id>
2024-03-18 19:38:18 +0700

    membuat prosedur output

--- 238_Insertionsort.cpp
+++ 238_Insertionsort.cpp
@@ -47,3 +47,15 @@ void insertionSort() {
 		arr[j + 1] = temp; //step 5
 	}
 }
+
+void display() {
+	cout << endl;
+	cout << "=========================" << endl;
+	cout << "Elemen array yang telah tersusun" << endl;
+	cout << "=========================" << endl;
+	for (int j = 0; j < n; j++) {
+		cout << arr[j] << endl;
+	}
+	cout << "Jumlah pass = " << n - 1 << endl;
+	cout << endl;
+}
